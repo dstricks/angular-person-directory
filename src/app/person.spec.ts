2d6435e3b20805329e777ae73e0a7989089a2f2d
David M. Strickland <dstricks@gmail.com>
2019-10-08 20:09:33 -0400

    Additional test for person

--- src/app/person.spec.ts
+++ src/app/person.spec.ts
@@ -4,4 +4,9 @@ describe('Person', () => {
   it('should create an instance', () => {
     expect(new Person()).toBeTruthy();
   });
+
+  it('should populate values passed into constructore', () => {
+    let person = new Person({title: "Operations Manager"});
+    expect(person.title).toEqual('Operations Manager');
+  });
 });
